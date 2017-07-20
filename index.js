d7ac8aa078ca5823cbdda04d47adee6dfa236c64
unknown <kyledijkstra1@gmail.com>
2017-07-20 09:50:26 -0400

    more setup

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
-// var gameOne = {
-//   time: "8:00am",
-//   home: " ",
-//   away: " "
-// };
-const express = require('express');
-const app = express();
-// app.use(express.static(__dirname));
-app.get('/', function(req, res) {
-   res.send('hello world');
-})
-app.listen(3000);
-console.log("listening...");
+var gameOne = {
+  time: "8:00am",
+  home: " ",
+  away: " "
+};
